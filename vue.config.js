a7dae13656b334533f95602e68cb1eec08d5a357
gamma <mail@gamma410.win>
2023-02-26 12:05:38 +0900

    Add files via upload

--- vue.config.js
+++ vue.config.js
@@ -2,3 +2,11 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
+
+module.exports = {
+  pwa: {
+    iconPaths: {
+      favicon32: 'public/img/icon.png',
+    }
+  }
+};
